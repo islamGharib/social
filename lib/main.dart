b048aeaac3b070fe17e7f2d1ea2a51f2ced94557
islamGharib <islamahmed1992@yahoo.com>
2022-02-15 10:41:21 +0200

    adding bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:social_app/modules/shop_login/social_login_screen.dart';
+import 'package:social_app/shared/bloc_provider.dart';
 import 'package:social_app/shared/styles/themes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   await Firebase.initializeApp();
 
   runApp(MyApp());
