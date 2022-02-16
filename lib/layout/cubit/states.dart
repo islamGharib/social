06fac95e392f7c043203608cd477a9b602eb0eda
islamGharib <islamahmed1992@yahoo.com>
2022-02-16 13:57:01 +0200

    adding social states

---
+++ lib/layout/cubit/states.dart
@@ -0,0 +1,8 @@
+
+abstract class SocialStates{}
+class SocialGetUserLoadingState extends SocialStates{}
+class SocialGetUserSuccessState extends SocialStates{}
+class SocialGetUserErrorState extends SocialStates{
+  final String error;
+  SocialGetUserErrorState(this.error);
+}
