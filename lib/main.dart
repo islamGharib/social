c52ac6789e41e505b849eda529fe6c36e9c60bb3
islamGharib <islamahmed1992@yahoo.com>
2022-02-16 12:41:03 +0200

    saving FirebaseAuth uId into shared preference to save home layout screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:social_app/modules/shop_login/social_login_screen.dart';
 import 'package:social_app/shared/bloc_provider.dart';
+import 'package:social_app/shared/component/constants.dart';
+import 'package:social_app/shared/network/local/cach_helper.dart';
 import 'package:social_app/shared/styles/themes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'layout/social_layout_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await Firebase.initializeApp();
+  await CachHelper.init();
+  uId = CachHelper.getData(key: 'uId');
+  // print(uId);
+  Widget widget;
+  if(uId != null) widget = SocialLayoutScreen();
+  else widget = SocialLoginScreen();
 
-  runApp(MyApp());
+  runApp(MyApp(widget));
 }
 
 class MyApp extends StatelessWidget {
+  Widget widget;
+  MyApp(this.widget);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      home: SocialLoginScreen(),
+      home: widget,
     );
   }
 }
--- lib/modules/shop_login/cubit/states.dart
+++ lib/modules/shop_login/cubit/states.dart
@@ -2,7 +2,10 @@
 abstract class SocialLoginState{}
 class SocialLogininitialState extends SocialLoginState{}
 class SocialLoginLoadingState extends SocialLoginState{}
-class SocialLoginSuccessState extends SocialLoginState{}
+class SocialLoginSuccessState extends SocialLoginState{
+  final String uId;
+  SocialLoginSuccessState(this.uId);
+}
 class SocialLoginErrorState extends SocialLoginState{
   final dynamic error;
   SocialLoginErrorState(this.error);
